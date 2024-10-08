a43dd824652360e480a6c3faee8f0d761aff6cf0
kieselll <popovicikirill95@gmail.com>
2024-10-08 20:14:17 +0300

    Create Problema 3.cpp

---
+++ 02.10.2024/Problema 3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x, a;
+    a = 1;
+    cout << "Numarul de linii este "; cin >> x;
+    for (int i = x; i > 0; i--) {
+        for (int n = i; n >= 0; n--) {
+            cout << " ";
+        }
+        for (int m = 1; m <= a; m++) {
+            cout << "*";
+        }
+        cout << endl;
+        a += 2;
+    }
+    system("pause");
+    return 0;
+}
